a82bced8e6498db95fd53f0baf7a280724110424
itheima <itheima@itcast.cn>
2020-11-23 15:32:29 +0800

    完成了用户功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -9,7 +9,7 @@ $(function () {
         }
         //全局统一挂载complete回调函数
         options.complete = function (res) {
-             console.log(res);
+           
             if (res.responseJSON.status === 1 && res.responseJSON.message == "身份认证失败！") {
                 //强制清空token
               
